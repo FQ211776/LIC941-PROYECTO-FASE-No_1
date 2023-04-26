9bc5df368f2138480572d8e627495b0601bdf047
Roberto Flores <fq211776@alumno.udb.edu.sv>
2023-04-25 18:01:23 -0600

    close #6; :white_check_mark: Agregado sección bienvenida

--- Pokemon_Bank_Fase_01/src/assets/index.js
+++ Pokemon_Bank_Fase_01/src/assets/index.js
@@ -1,5 +1,9 @@
 import logo from "./logo.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
-export { logo, menu, close };
+//
+import robot from "./robot.png";
+import discount from "./Discount.svg";
+import arrowUp from "./arrow-up.svg";
+//
+export { logo, menu, close, robot, discount, arrowUp };
